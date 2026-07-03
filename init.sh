237bc75262c14c52bca75b8cd9c0fae2061c12a6
Elyas Bianchi <elyasb99@gmail.com>
2026-07-03 06:46:32 -0700

    waybar

--- init.sh
+++ init.sh
@@ -2,11 +2,14 @@
 NVIM="$PWD/nvim"
 TMUX="$PWD/.tmux.conf"
 HYPR="$PWD/hypr"
+WAYBAR="$PWD/waybar"
 
 rm -rf ~/.config/nvim
 rm -rf ~/.tmux.conf
 rm -rf ~/.config/hypr
+rm -rf ~/.config/waybar
 
 ln -s "$NVIM" ~/.config/nvim
 ln -s "$TMUX" ~/.tmux.conf
 ln -s "$HYPR" ~/.config/hypr
+ln -s "$WAYBAR" ~/.config/waybar
