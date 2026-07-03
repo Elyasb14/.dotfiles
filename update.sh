2e893dd7fef99ad0b1c7a2f392d1b30f32ffe4b2
Elyas Bianchi <elyasb99@gmail.com>
2026-07-02 20:02:39 -0700

    more hyprland

--- update.sh
+++ update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 NVIM="$PWD/nvim"
 TMUX="$PWD/.tmux.conf"
+HYPR="$PWD/hypr"
 
 rm -rf ~/.config/nvim
 rm -f ~/.tmux.conf
+rm -rf ~/.config/hypr
 
 ln -s "$NVIM" ~/.config/nvim
 ln -s "$TMUX" ~/.tmux.conf
+ln -s "$HYPR" ~/.config/hypr
