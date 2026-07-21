eaa6f4879c6f164518b7b1ace97aea2929c09112
Elyas Bianchi <elyasb99@gmail.com>
2026-07-21 13:20:51 -0700

    bashrc

--- init.sh
+++ init.sh
@@ -3,13 +3,16 @@ NVIM="$PWD/nvim"
 TMUX="$PWD/.tmux.conf"
 HYPR="$PWD/hypr"
 WAYBAR="$PWD/waybar"
+BASHRC="$PWD/.bashrc"
 
 rm -rf ~/.config/nvim
 rm -rf ~/.tmux.conf
 rm -rf ~/.config/hypr
 rm -rf ~/.config/waybar
+rm -rf ~/.bashrc
 
 ln -s "$NVIM" ~/.config/nvim
 ln -s "$TMUX" ~/.tmux.conf
 ln -s "$HYPR" ~/.config/hypr
 ln -s "$WAYBAR" ~/.config/waybar
+ln -s "$BASHRC" ~/.bashrc
